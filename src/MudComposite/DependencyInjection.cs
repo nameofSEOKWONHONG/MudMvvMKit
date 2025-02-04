a418ed54d189875677c66cc0abca5fef96f190a0
seokwon <h20913@gmail.com>
2025-02-04 17:25:35 +0900

    curcuithandler로 이중 로그인 방지는 불가능. 일단 선순위 로그인만 유지하는 것으로 함.

--- src/MudComposite/DependencyInjection.cs
+++ src/MudComposite/DependencyInjection.cs
@@ -7,7 +7,6 @@ public static class DependencyInjection
 {
     public static void AddMudComposite(this IServiceCollection services)
     {
-        services.AddScoped<IJsWindow, JsWindow>();
         services.AddScoped<MudViewModelItem>();
     }
 }
